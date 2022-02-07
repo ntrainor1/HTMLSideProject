5e07a4ced1c384aded909b3bd4d72b80ebc87151
Nathaniel Trainor <natetrainor@msn.com>
2022-02-07 11:00:55 -0600

    Adding store page

--- js/myscripts.js
+++ js/myscripts.js
@@ -8,10 +8,11 @@ $(function() {
   });
 });
 
-function show(shown, hidden, hidden2) {
+function show(shown, hidden, hidden2, hidden3) {
   document.getElementById(shown).style.display='block';
   document.getElementById(hidden).style.display='none';
   document.getElementById(hidden2).style.display='none';
+  document.getElementById(hidden3).style.display='none';
   return false;
 }
 
